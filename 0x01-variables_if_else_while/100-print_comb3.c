f9d4ec8c003cca0cda3e48c2da239ae89158bf3c
Gloriariechi99 <gloriariechi99@gmail.com>
2023-02-17 18:46:59 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,17 +8,21 @@ int main(void)
 {
 	int dig1, dig2;
 
-	for (dig2 = dig1 + 1; dig2 < 10; dig2++)
+	for (dig1 = 0; dig1 < 9; dig1++)
 	{
-		putchar((dig1 % 10) + '0');
-		putchar((dig2 % 10) + '0');
 
-		if (dig1 == 8 && dig2 == 9)
+		for (dig2 = dig1 + 1; dig2 < 10; dig2++)
+		{
+			putchar((dig1 % 10) + '0');
+			putchar((dig2 % 10) + '0');
+
+			if (dig1 == 8 && dig2 == 9)
 			continue;
 
-		putchar(',');
-		putchar(' ');
+			putchar(',');
+			putchar(' ');
 
+		}
 	}
 
 	putchar('\n');
