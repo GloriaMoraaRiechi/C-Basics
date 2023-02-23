86474ce1c17d61347842d7227957e5ee1772fe60
Gloriariechi99 <gloriariechi99@gmail.com>
2023-02-23 12:57:47 -0800

    added spaces

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,13 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 3 == 0 && num % 5 != 0)
-			printf("Fizz");
+			printf(" Fizz");
 
 		else if (num % 5 == 0 && num % 3 != 0)
-			printf("Buzz");
+			printf(" Buzz");
 
 		else if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 
 		else if (num == 1)
 		{
@@ -28,7 +28,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%d", num);
+			printf(" %d", num);
 		}
 
 	}
