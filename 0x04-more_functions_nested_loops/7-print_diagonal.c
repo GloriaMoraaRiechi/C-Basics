c4625c92fc0cf12715536b75576b7f04611ef920
Gloriariechi99 <gloriariechi99@gmail.com>
2023-02-23 12:08:45 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,14 +9,20 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (space = 0; space < len; space++)
-			_putchar(' ');
-		_putchar('\\');
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+				_putchar(' ');
+			_putchar('\\');
 
-		if (len == n - 1)
-			continue;
+			if (len == n - 1)
+				continue;
 
-		_putchar('\n');
+			_putchar('\n');
+
+		}
 
 	}
+
+	_putchar('\n');
 }
